8073c0ceba0413ee09404871662fb628c4e301ce
Dhanraj-Patil <69854547+Dhanraj-Patil@users.noreply.github.com>
2021-10-02 22:52:06 +0530

    Update Encrypt.java
    Enter the word
    Dhanraj
    Enter the key of encryption
    1
    Encrypted word  =Eibosbk
    Enter the key of decryption
    1
    Decrypted word   =Dhanraj

--- Encrypt.java
+++ Encrypt.java
@@ -10,7 +10,7 @@ String s = sc.nextLine();
 System.out.println("Enter the key of encryption");
 int ek = sc.nextInt();
 String ss = "",sp = "";
-for(int i=0;i<s.length();i--)
+for(int i=0;i<s.length();i++)
 {
 char d = s.charAt(i);
 int p =(int)d;
