72866eb0fb5323774bd61c7ae9e08b5f803d43de
v30tyagi <92777148+v30tyagi@users.noreply.github.com>
2021-10-19 11:52:19 +0530

    Update Encrypt.java

--- Encrypt.java
+++ Encrypt.java
@@ -7,7 +7,7 @@ public static void main(String args[])
 Scanner sc= new Scanner(System.in);
 System.out.println("Enter the word");
 String s = sc.nextLine();
-System.out.println("Enter the key of encryption:");
+
 int ek = sc.nextInt();
 String ss = "",sp = "";
 for(int i=0;i<s.length();i--)
@@ -19,7 +19,6 @@ d = (char)p;
 ss = ss+d;
 }
 System.out.println("Encrypted word:  ="+ss);
-System.out.println("Enter the key of decryption as :");
 int dk = sc.nextInt();				
 if(ek==dk)
 {
